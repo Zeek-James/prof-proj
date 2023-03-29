e0947e298ee5fbbd67c016f7d08bd789b7db26ca
Isaac James <chizuruokej@yahoo.com>
2023-03-29 10:37:18 +0100

    Adds normalize to GlobalStyle css

--- styles/GlobalStyles.ts
+++ styles/GlobalStyles.ts
@@ -1,6 +1,9 @@
 import { createGlobalStyle } from "styled-components";
+import normalize from "styled-normalize";
 
 export const GlobalStyles = createGlobalStyle`
+    ${normalize}
+
     @font-face{
         font-family:"Inter";
         src: url("/fonts/Inter.ttf");
